4def07f01a2309f0ea323d880472e9256e48085a
Ziad Emad <engziad90@gmail.com>
2024-07-18 21:51:56 +0200

    added account controller and register

---
+++ api/Migrations/20240718194528_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "09c836e3-991a-4410-81e3-be568517fc51", null, "User", "USER" },
+                    { "42ad8a82-4192-4bfc-acc8-a1921abea26e", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "09c836e3-991a-4410-81e3-be568517fc51");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "42ad8a82-4192-4bfc-acc8-a1921abea26e");
+        }
+    }
+}
